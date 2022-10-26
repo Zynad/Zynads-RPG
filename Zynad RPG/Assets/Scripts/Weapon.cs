13ca47c20e41bf9a559f2794a6b6c8dc8d0fbaef
Zynad <zynadlol@gmail.com>
2022-10-26 14:57:19 +0200

    Enemy system

--- Zynad RPG/Assets/Scripts/Weapon.cs
+++ Zynad RPG/Assets/Scripts/Weapon.cs
@@ -41,9 +41,16 @@ public class Weapon : Collidable
         {
             if (coll.name == "Player")
             return;
-                
-            
-            Debug.Log(coll.name);
+
+
+            //Create a new damage object, then we wiill send it to the fighter we've hit
+            Damage dmg = new Damage
+            {
+                damageAmount = damagePoint,
+                origin = transform.position,
+                pushForce = pushForce
+            };
+            coll.SendMessage("RecieveDamage", dmg);
             
         }
         
